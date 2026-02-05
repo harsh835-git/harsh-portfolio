3d665bcea223f8f01470ad3f5aa1b1be0a9a13c4
harsh835-git <harshsoni83588@gmail.com>
2026-02-05 10:47:45 +0530

    fix: removed conflict markers from vite config

--- harsh-portfolio/vite.config.js
+++ harsh-portfolio/vite.config.js
@@ -2,13 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-<<<<<<< HEAD
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-=======
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/",
->>>>>>> 51d2da78f3d77e58a0540d2b3476bb8966fea6d9
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
